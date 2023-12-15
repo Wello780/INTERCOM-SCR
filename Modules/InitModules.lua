cd5a1574c8c1a221a6aad6dc3a7b4ef9d7e7f682
13B8B <108516478+reed192512@users.noreply.github.com>
2023-12-15 12:12:37 +0800

    Update InitModules.lua

--- Modules/InitModules.lua
+++ Modules/InitModules.lua
@@ -3,7 +3,7 @@ return {
     init = function(callback)
         if callback == nil then callback = function() end end;
         local moduleLinks = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://raw.githubusercontent.com/reed192512/INTERCOM-SCR"..
-        (_G.EzHubDevMode and "dev" or "master" or "main").."/Modules/Modules.json"));
+        (_G.EzHubDevMode and "dev" or "master").."/Modules/Modules.json"));
 
         local moduleIndex = 0;
         local moduleNumber = (function()
